07579cc0bff0d90bb36eda55854f965dcfdf284e
Abdallha Hamdy <43465167+abdallhahamdy@users.noreply.github.com>
2023-04-23 16:09:31 +0200

    create Student model Closes #1

---
+++ back-end/src/main/java/com/spring/student/model/Student.java
@@ -0,0 +1,25 @@
+package com.spring.student.model;
+
+
+import com.spring.student.model.enums.Gender;
+
+import javax.xml.crypto.Data;
+
+public class Student {
+
+    private int id;
+
+    private String FullName;
+
+    private Gender gender;
+
+    private String phone;
+
+    private String age;
+
+    private String address;
+
+    private Data dataCreated;
+
+    private Data dataUpdated;
+}
