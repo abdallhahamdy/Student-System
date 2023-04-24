270e2e7aa97aff44d848809c0d11f4c6d0b206b5
Abdallha Hamdy <43465167+abdallhahamdy@users.noreply.github.com>
2023-04-24 23:31:35 +0200

    Create Repository Closes #5

---
+++ back-end/src/main/java/com/spring/student/dao/StudentRepository.java
@@ -0,0 +1,17 @@
+package com.spring.student.dao;
+
+import com.spring.student.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
+/*
+http://localhost:8080/students   GET
+http://localhost:8080/students/1  GET
+http://localhost:8080/students  POST
+http://localhost:8080/students/1  DELETE
+http://localhost:8080/students/1  PUT
+ */
