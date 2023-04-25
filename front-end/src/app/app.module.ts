feed697e3761a1326d082027a7a5a73d8944afc5
Abdallha Hamdy <43465167+abdallhahamdy@users.noreply.github.com>
2023-04-25 15:04:11 +0200

    create students componenet

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { RegisterComponent } from './components/register/register.component';
+import { StudentsComponent } from './components/students/students.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    RegisterComponent
+    RegisterComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule,
