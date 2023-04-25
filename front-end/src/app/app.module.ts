d527aca5afa722245fe82ea9bd58ed84be211fec
Abdallha Hamdy <43465167+abdallhahamdy@users.noreply.github.com>
2023-04-25 15:53:12 +0200

    create options components and editing header

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { RegisterComponent } from './components/register/register.component';
 import { StudentsComponent } from './components/students/students.component';
+import { OptionsComponent } from './components/options/options.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { StudentsComponent } from './components/students/students.component';
     HeaderComponent,
     FooterComponent,
     RegisterComponent,
-    StudentsComponent
+    StudentsComponent,
+    OptionsComponent
   ],
   imports: [
     BrowserModule,
