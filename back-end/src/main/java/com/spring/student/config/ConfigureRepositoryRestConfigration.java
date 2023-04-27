64bfa8af93bda5864ca25696c196f37730242258
Abdallha Hamdy <43465167+abdallhahamdy@users.noreply.github.com>
2023-04-27 02:41:45 +0200

    add ids to entites

---
+++ back-end/src/main/java/com/spring/student/config/ConfigureRepositoryRestConfigration.java
@@ -0,0 +1,24 @@
+package com.spring.student.config;
+
+import com.spring.student.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Configuration
+public class ConfigureRepositoryRestConfigration implements RepositoryRestConfigurer  {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(Student.class);
+    }
+    
+}
