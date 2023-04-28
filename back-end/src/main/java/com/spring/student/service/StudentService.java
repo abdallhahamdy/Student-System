1cca609837828745c9afd8efc14ad35aac942749
Abdallha Hamdy <43465167+abdallhahamdy@users.noreply.github.com>
2023-04-28 22:55:21 +0200

    spring (getallStudents - getStudent)

--- back-end/src/main/java/com/spring/student/service/StudentService.java
+++ back-end/src/main/java/com/spring/student/service/StudentService.java
@@ -1,8 +1,11 @@
 package com.spring.student.service;
 
 import com.spring.student.dao.StudentRepository;
+import com.spring.student.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StudentService {
@@ -11,5 +14,12 @@ public class StudentService {
     @Autowired
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getStudents(){
+        return studentRepository.findAll();
+    }
+    public Student getStudentById (Long id){
+        return studentRepository.findById(id).get();
     }
 }
