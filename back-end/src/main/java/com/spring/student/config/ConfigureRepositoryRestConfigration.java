37b0e458631ad4c6eb85f6d94bbb8c782e7f6868
Abdallha Hamdy <43465167+abdallhahamdy@users.noreply.github.com>
2023-04-29 22:28:07 +0200

    Disabled Method (Get,Post,Delete,Put)

--- back-end/src/main/java/com/spring/student/config/ConfigureRepositoryRestConfigration.java
+++ back-end/src/main/java/com/spring/student/config/ConfigureRepositoryRestConfigration.java
@@ -6,19 +6,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-
-import javax.persistence.EntityManager;
-import javax.persistence.metamodel.EntityType;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class ConfigureRepositoryRestConfigration implements RepositoryRestConfigurer  {
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] unsupported = {HttpMethod.GET,HttpMethod.POST,HttpMethod.DELETE,HttpMethod.PUT};
+        displeHttpMethod(Student.class,config,unsupported);
         config.exposeIdsFor(Student.class);
     }
 
+    public void displeHttpMethod(Class theClass, RepositoryRestConfiguration config, HttpMethod[] unsupportedMethod) {
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedMethod)))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedMethod));
+    }
 }
--- back-end/src/main/java/com/spring/student/config/ConfigureRepositoryRestConfigration1.java
+++
@@ -1,7 +0,0 @@
-package com.spring.student.config;
-
-import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-
-public interface ConfigureRepositoryRestConfigration1 {
-    void configureRepositoryRestConfiguration(RepositoryRestConfiguration config);
-}
